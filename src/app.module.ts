4a9ca2a7281f36d3c9369156ad854ebc6fffee18
Wocgc <choigyucheol@naver.com>
2022-09-21 12:28:45 +0900

    created dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 
-
 @Module({
   imports: [],
   controllers: [MoviesController],
